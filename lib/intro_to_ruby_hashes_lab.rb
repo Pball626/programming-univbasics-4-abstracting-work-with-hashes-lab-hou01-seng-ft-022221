0e45a7e13cb7e494e46a980b4aa68ad1c06c2de7
Jose Mendoza <pball626@gmail.com>
2021-02-08 01:04:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   {
-    :name => "joey"
+    :name => "Grace Hopper"
   }
   # return a hash that includes the key and value parameters passed into this method
 end
